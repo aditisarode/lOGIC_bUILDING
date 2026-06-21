272d2d001483bba053b22c5586cbd99471930cff
aditisarode <aditids2006@gmail.com>
2026-06-21 20:31:56 +0530

    Problem 30 Solution

---
+++ program30.c
@@ -0,0 +1,27 @@
+//objective-> 
+
+#include<stdio.h>
+
+void Display(int iCnt,int n)
+{
+    //filter
+    if(iCnt<0)
+    {
+        printf("Invalid input\n");
+        return;
+    }
+    for(iCnt=1;iCnt<=n;iCnt++)
+    {
+        printf("Jay Ganesh..\n");
+    }
+}
+int main()
+{
+    int iCnt=0;
+    int n;
+
+    printf("Enter number of times u want to print on the screen :\n");
+    scanf("%d",&n);
+
+    Display(iCnt,n);
+}
