f5aa6fccd0a689adec88a8f3450b71b94137d196
aditisarode <aditids2006@gmail.com>
2026-06-22 20:27:47 +0530

    Problem 67 Solution

---
+++ program67.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class NumberX  
+{
+  public boolean CheckPrime(int iNo)
+  {
+    // Logic 
+    int iCnt = 0;
+    
+    for(iCnt = 2; iCnt <= (iNo/2); iCnt++)
+    {
+      if ((iNo % iCnt )== 0)
+      {
+        return false;                    // Bad programming
+      }
+    }
+    return true;
+  }   
+}
+class program67
+{
+       public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+        boolean bRet = false;
+
+        System.out.println("Enter Number:");
+        iValue = sobj.nextInt();
+
+        NumberX nobj = new NumberX();
+
+         bRet = nobj.CheckPrime(iValue);
+
+        if (bRet == true)
+        {
+            System.out.println("Is Prime: " +bRet);
+        }
+        else{
+            System.out.println("Not Prime:" +bRet);
+        }
+    }
+}
+
+
+// Time Complexity: O(N/2)
+// Where N>=0
