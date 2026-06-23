a2db47e4f718bc9706e2f72a524d1949ced60661
aditisarode <aditids2006@gmail.com>
2026-06-23 18:35:58 +0530

    Problem 101 Solution

---
+++ program101.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int Arr[5] = {0};
+    int iCnt = 0;
+    
+    printf("Enter the elemnets :\n");
+
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        scanf("%d\n",&Arr[iCnt]);
+    }
+    printf("Elements of Array are ;\n");
+
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
