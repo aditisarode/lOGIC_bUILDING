bcb9ca4807bcbce7b3f8788819f810383d122acc
aditisarode <aditids2006@gmail.com>
2026-06-27 19:22:38 +0530

    Program 219 Solution

---
+++ program219.java
@@ -0,0 +1,67 @@
+/*
+    iRow = 4
+    iCol = 4
+
+    @  #  #  #
+    %  @  #  #       
+    %  %  @  #   
+    %  %  %  @    
+    
+*/
+
+import java.util.*;                            
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+
+        // Filter for diagonal pattern
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid Parameters");
+            System.out.println("Number of rows and columns should be same.");
+            return;
+        }
+        for(i = 1; i <= iRow ; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+              if(i > j)                
+              {
+                System.out.print("%\t");
+              }
+              else if(i == j)
+              {
+                System.out.print("@\t");
+              }
+              else
+              {
+                System.out.print("#\t");
+              }
+            }
+        System.out.println();
+    }
+}
+}
+class program219
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0;
+        int iValue2 = 0;
+        
+        System.out.println("Enter the number of rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns :");
+        iValue2 = sobj.nextInt();
+        
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+    }
+}
