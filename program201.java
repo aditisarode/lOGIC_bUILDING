d047fa7eac075dcec29c171856889d5f2f166d38
aditisarode <aditids2006@gmail.com>
2026-06-27 19:15:46 +0530

    Program 201 Solution

---
+++ program201.java
@@ -0,0 +1,61 @@
+/*
+    iRow = 4
+    iCol = 4
+
+    $ $ $ $
+    # # # #
+    $ $ $ $
+    # # # #
+
+*/
+
+import java.util.*;                               // if-else = 4 times run
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+
+        for(i = 1; i<= iRow ; i++)
+        {
+        if((i % 2) == 0)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print("#\t");
+
+            }
+        }
+        else
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print("$\t");
+
+            }
+        }
+            System.out.println();
+        }
+    }
+}
+class program201
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0;
+        int iValue2 = 0;
+        
+        System.out.println("Enter the number of rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns :");
+        iValue2 = sobj.nextInt();
+        
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+    }
+}
