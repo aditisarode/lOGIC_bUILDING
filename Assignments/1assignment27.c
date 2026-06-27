cc08efc504a3727be030e587c565059fa2227dbf
aditisarode <aditids2006@gmail.com>
2026-06-27 19:28:20 +0530

    Program 1 Solution

---
+++ Assignments/1assignment27.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+   
+     if(iRow != iCol)
+        {
+            printf("Invalid Parameters\n");
+            printf("Number of rows and columns should be same.\n");
+            return;
+        }
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            if(i == j)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows : ");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of columns : ");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
