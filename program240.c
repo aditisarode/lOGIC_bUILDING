0a99bd2ee007a16c3c280577c7cf0754f5758810
aditisarode <aditids2006@gmail.com>
2026-06-28 19:10:24 +0530

    Problem 240 Solution

---
+++ program240.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string :\n");
+    scanf("%s",Arr);                          // Issue
+
+    printf("Entered string is : %s\n", Arr);
+
+    return 0;
+}
