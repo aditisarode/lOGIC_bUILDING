032028011d7e264256bbf1649523c588b92a6c63
aditisarode <aditids2006@gmail.com>
2026-06-28 19:16:05 +0530

    Problem 3 Solution

---
+++ Assignments/3assignment28.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkDigit(char ch)
+{
+    if(ch >= 48 && ch <= 57)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character : ");
+    scanf("%c", &cValue);
+
+    bRet = ChkDigit(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is digit.");
+    }
+    else
+    {
+        printf("It is not a digit.");
+    }
+
+    return 0;
+}
