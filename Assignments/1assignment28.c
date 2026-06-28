09ff94888e1f27e0e80ac263efa856b5b1c69c6a
aditisarode <aditids2006@gmail.com>
2026-06-28 19:15:33 +0530

    Problem 1 Solution

---
+++ Assignments/1assignment28.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkAlpha(char ch)
+{
+    if((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character : ");
+    scanf("%c", &cValue);
+
+    bRet = ChkAlpha(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is alphabet.");
+    }
+    else
+    {
+        printf("It is not a alphabet.");
+    }
+
+    return 0;
+}
