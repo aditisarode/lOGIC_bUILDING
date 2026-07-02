f210d6682457b368c9af7e7ae39fab3a888c3152
aditisarode <aditids2006@gmail.com>
2026-07-02 16:02:36 +0530

    Problem 318 Solution

---
+++ program318.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+typedef unsigned int UINT;
+
+int main()
+{
+    UINT iMask = 0x1;
+    int iCnt = 0;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+        
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);
+    iMask = iMask << 1;
+
+    return 0;
+}
