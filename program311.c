9300eb9668b7e5cc7a830f551ebc15c6c1a17edb
aditisarode <aditids2006@gmail.com>
2026-07-02 16:00:15 +0530

    Problem 311 Solution

---
+++ program311.c
@@ -0,0 +1,25 @@
+#include <Stdio.h>
+
+typedef unsigned int UINT;
+
+int main()
+{
+    UINT iNo = 0;
+    UINT iMask = 64;             // 7th bit    
+    UINT iAns = 0;
+
+    printf("Enter the number :\n");
+    scanf("%d",&iNo);
+    
+    iAns = iNo & iMask;
+
+    if(iAns == iMask)
+    {
+        printf("7th bit is ON\n");
+    }
+    else
+    {
+        printf("7th bit is OFF");
+    }
+    return 0;
+}
